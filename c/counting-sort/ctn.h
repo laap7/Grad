814c8b5b9fde1509471377d85a283fa97d0f3486
laap7 <154691421+laap7@users.noreply.github.com>
2023-12-25 17:39:17 +0100

    Add files via upload

---
+++ c/counting-sort/ctn.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_INCLUDED
+#define MAIN_INCLUDED
+
+int *LerVetor(int b);
+int *CriarVetor(int b);
+int *CountingSort(int *a, int n, int k);
+void LiberarVetor(int *a);
+void PrintVetor(int *a,int b);
+
+
+#endif //MAIN_INCLUDED
