c16d997637b9850faec8c6410b2a7ca3891d545d
laap7 <154691421+laap7@users.noreply.github.com>
2023-12-25 17:34:13 +0100

    Add files via upload

---
+++ c/quick-shell-sort/qs.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_INCLUDED
+#define MAIN_INCLUDED
+
+int LerTamanhoVetor();
+int *LerVetor(int b);
+void LiberarVetor(int *a);
+int *ExtrairVetor(int *a, int b);
+void ShellSort(int *a, int b);
+void Quick(int *a, int b);
+void QuickSort(int *a, int c, int f);
+void PrintVetor(int *a,int b);
+void ChecarOrdenacao(int *a, int b);
+
+
+#endif //MAIN_INCLUDED
